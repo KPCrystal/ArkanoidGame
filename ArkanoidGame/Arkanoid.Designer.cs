54bc4f099a46086baca456a5536c7245c0bc9acc
KPCrystal <kpcrystal01@gmail.com>
2020-10-14 19:03:42 +0900

    Ver.1.0
    ArkanoidGame Ver.1.0

---
+++ ArkanoidGame/Arkanoid.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace ArkanoidGame
+{
+    partial class Arkanoid
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Arkanoid));
+            this.lblScore = new System.Windows.Forms.Label();
+            this.tm = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(5, 8);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(88, 26);
+            this.lblScore.TabIndex = 0;
+            this.lblScore.Text = "Score: 0";
+            // 
+            // tm
+            // 
+            this.tm.Interval = 5;
+            this.tm.Tick += new System.EventHandler(this.tm_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ArkanoidGame.Properties.Resources.company_logo;
+            this.pictureBox1.Location = new System.Drawing.Point(1, 416);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(72, 49);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Arkanoid
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 26F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Snow;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(730, 500);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblScore);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Arkanoid";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Arkanoid_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Timer tm;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ ArkanoidGame/Arkanoid.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ArkanoidGame
+{
+    public partial class Arkanoid : Form
+    {
+        PictureBox _paddle, _ball;
+
+        int _paddleDx = 10;                 //パドルポジション
+        int dx = 2;                         //X ポジション
+        int dy = 2;                         //Y ポジション
+        int score = 0;                      //スコア
+        int live = 3;                       //命
+
+        List<PictureBox> _hearts;
+
+        /// <summary>
+        /// 初期表示
+        /// </summary>
+        public Arkanoid()
+        {
+            InitializeComponent();
+            Intialize();
+        }
+
+        #region イベント
+        /// <summary>
+        /// キーダウンイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Arkanoid_KeyDown(object sender, KeyEventArgs e)
+        {
+            int left = _paddle.Location.X;
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    left -= _paddleDx;
+                    break;
+                case Keys.Right:
+                    left += _paddleDx;
+                    break;
+                case Keys.Space:
+                    tm.Start();
+                    break;
+                case Keys.Escape:
+                    tm.Stop();
+                    DialogResult result = MessageBox.Show("Do you want to exit?", "Game Pause",
+                        MessageBoxButtons.YesNo);
+                    if (result == DialogResult.Yes)
+                    {
+                        Application.Exit();
+                    }
+                    else
+                    {
+                        tm.Start();
+                    }
+                    break;
+            }
+            if (left >= 0 && left <= Width - _paddle.Width)
+            {
+                _paddle.Left = left;
+                if (!tm.Enabled)
+                    _ball.Left = left + _paddle.Width / 2 - _ball.Width / 2;
+            }
+        }
+
+        /// <summary>
+        /// タイマーイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tm_Tick(object sender, EventArgs e)
+        {
+            dx = _ball.Left <= 0 || _ball.Left >= Width - _ball.Width ? -dx : dx;
+            dy = _ball.Top <= 0 ? -dy : dy;
+
+            // ボールをハンドル
+            if (_ball.Top >= Height)
+            {
+                tm.Stop();
+                _ball.Left = _paddle.Left + _paddle.Width / 2 - _ball.Width / 2;
+                _ball.Top = _paddle.Top - _ball.Height;
+                _hearts[live - 1].Image = Properties.Resources.dark_heart;
+                live -= 1;
+                if (live < 1)
+                {
+                    MessageBox.Show("Game over! You Lose.");
+                    Controls.Clear();
+                }
+            }
+
+            _ball.Left -= dx; _ball.Top -= dy;
+            foreach (Control c in Controls)
+                if (c.Tag != null && _ball.Bounds.IntersectsWith(c.Bounds))
+                {
+                    caldy(c); caldx(c);
+                    Controls.Remove(c);
+                    score += 10;
+                    lblScore.Text = "Score: " + score.ToString();
+                }
+
+            if (_ball.Bounds.IntersectsWith(_paddle.Bounds))
+            {
+                dy = Math.Abs(dy); caldx(_paddle);
+            }
+            if (score == 9 * 5 * 10) win();
+        }
+        #endregion
+
+        #region メソッド
+        /// <summary>
+        /// ゲームの初期化設定
+        /// </summary>
+        void Intialize()
+        {
+            createPaddle();
+            createBall();
+            drawBlocks();
+            createHearts();
+        }
+
+        /// <summary>
+        /// パドルを作成
+        /// </summary>
+        void createPaddle()
+        {
+            _paddle = new PictureBox();
+            _paddle.Size = new Size(140, 20);
+            _paddle.SizeMode = PictureBoxSizeMode.StretchImage;
+            _paddle.Image = Properties.Resources.paddle;
+            _paddle.Location = new Point(Width / 2 - _paddle.Width / 2
+                , Height - _paddle.Height);
+            Controls.Add(_paddle);
+        }
+
+        /// <summary>
+        /// ボールを作成
+        /// </summary>
+        void createBall()
+        {
+            _ball = new PictureBox();
+            _ball.Size = new Size(16, 16);
+            _ball.SizeMode = PictureBoxSizeMode.StretchImage;
+            _ball.Image = Properties.Resources.ball;
+            _ball.Location
+                = new Point(_paddle.Left + _paddle.Width / 2 - _ball.Width / 2
+                , _paddle.Top - _ball.Height);
+            Controls.Add(_ball);
+        }
+
+        /// <summary>
+        /// ブログを作成
+        /// </summary>
+        void drawBlocks()
+        {
+            Random rand = new Random();
+
+            List<Bitmap> blockImages = new List<Bitmap>
+            {
+                Properties.Resources.block01, Properties.Resources.block02,
+                Properties.Resources.block03, Properties.Resources.block04,
+                Properties.Resources.block05
+            };
+
+            // ブログのサイズを設定
+            blockResize(blockImages, 70, 30);
+
+            //ループして複数のブログを作成
+            for (int rows = 0; rows < 5; rows++)
+                for (int cols = 0; cols < 9; cols++)
+                {
+                    Label lblBlock = new Label();
+                    lblBlock.Tag = "brick";
+                    lblBlock.Location = new Point(50 + cols * 70, rows * 30 + 50);
+                    lblBlock.Size = new Size(70, 30);
+                    lblBlock.Image = Properties.Resources.paddle;
+                    lblBlock.Image
+                        = blockImages[rand.Next() % blockImages.Count];
+                    Controls.Add(lblBlock);
+                }
+        }
+
+        /// <summary>
+        /// 全ブログ打つしたら勝つ
+        /// </summary>
+        void win()
+        {
+            tm.Stop();
+            Controls.Clear();
+            MessageBox.Show("Congratulations, you win!");
+            score = 0; lblScore.Text = "Score: 0";
+            Controls.Add(lblScore);
+            Intialize();
+        }
+
+        /// <summary>
+        /// ボールのXポジションを設定
+        /// </summary>
+        /// <param name="c"></param>
+        void caldx(Control c)
+        {
+            dx = c.Left + c.Width / 2 < _ball.Left ? -Math.Abs(dx) : Math.Abs(dx);
+        }
+
+        /// <summary>
+        /// ボールのYポジションを設定
+        /// </summary>
+        /// <param name="c"></param>
+        void caldy(Control c)
+        {
+            dy = c.Top + c.Height / 2 < _ball.Top ? -Math.Abs(dy) : Math.Abs(dy);
+        }
+
+        /// <summary>
+        /// ブログのサイズを再設定
+        /// </summary>
+        /// <param name="images"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        void blockResize(List<Bitmap> images, int width, int height)
+        {
+            Graphics graph;
+            for (int i = 0; i < images.Count; i++)
+            {
+                Bitmap bmp = new Bitmap(width, height);
+                graph = Graphics.FromImage(bmp);
+                graph.DrawImage(images[i], 0, 0, width, height);
+                graph.Dispose();
+                images[i] = bmp;
+            }
+        }
+
+        /// <summary>
+        /// 命を表示するためハートを作成する
+        /// </summary>
+        void createHearts()
+        {
+            _hearts = new List<PictureBox>();
+            for (int i = 0; i < 3; i++)
+            {
+                PictureBox box = new PictureBox();
+                box.Size = new Size(30, 30);
+                box.Location = new Point(Width - (3 - i) * box.Width, 0);
+                box.SizeMode = PictureBoxSizeMode.StretchImage;
+                box.Image = Properties.Resources.heart;
+                Controls.Add(box); _hearts.Add(box);
+            }
+        }
+        #endregion
+    }
+}
